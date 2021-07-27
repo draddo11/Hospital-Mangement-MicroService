74a2b54a59e8f091d8711d1ea93be4daf4367e11
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-07-27 22:46:40 +0000

    added the eureka server to the project

---
+++ discovery-server/src/test/java/com/nana/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nana.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
