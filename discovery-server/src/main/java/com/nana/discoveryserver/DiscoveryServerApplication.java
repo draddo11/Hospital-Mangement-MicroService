c74b2411cc6a21de1115e74788bfee330be05d6a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-07-28 13:36:20 +0000

    added the discovery server by importing as an existing module

--- discovery-server/src/main/java/com/nana/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/nana/discoveryserver/DiscoveryServerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-@EnableEurekaServer
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
--- discovery-server/src/test/java/com/nana/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/nana/discoveryserver/DiscoveryServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.nana.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
+@EnableEurekaServer
 class DiscoveryServerApplicationTests {
 
     @Test
